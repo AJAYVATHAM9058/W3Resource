fd254e2599dc9dd1ee6041ce44115ee526cf1586
Ajay Vatham <ajayvatham31@gmail.com>
2024-10-04 01:43:18 +0530

    modifie

--- w3Resource/com/arrayprograms/Pr1SortNumArrStrArr.java
+++ w3Resource/com/arrayprograms/Pr1SortNumArrStrArr.java
@@ -20,19 +20,25 @@ public class Pr1SortNumArrStrArr {
 		for(int i=0;i<n;i++) {
 			arr[i] = rn.nextInt(100);
 		}
+		
 		System.out.println("Original Numeric Array : ");
 		System.out.println(Arrays.toString(arr));
+		
 		Arrays.sort(arr);
+		
 		System.out.println("Sorted Numeric Array : ");
 		System.out.println(Arrays.toString(arr));
 		
 		String[] arr1 = new String[n];
 		System.out.println("Enter Strings for array ");
+		
 		for(int i=0;i<n;i++) {
 			arr1[i] = sc.next();
 		}
+		
 		System.out.println("Original String Array ");
 		System.out.println(Arrays.toString(arr1));
+		
 		System.out.println("After sorting the array ");
 		Arrays.sort(arr1);
 		System.out.println(Arrays.toString(arr1));
